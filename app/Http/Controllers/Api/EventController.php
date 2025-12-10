8db404add3ca6a1721c168195b4f4f66dbad5d8c
Pavlina <pavlina.malinkova@email.cz>
2025-12-10 15:23:27 +0100

    finished chapter - Controlling JSON response

---
+++ app/Http/Controllers/Api/EventController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\EventResource;
+use App\Models\Event;
+use Illuminate\Http\Request;
+
+class EventController extends Controller
+{
+  /**
+   * Display a listing of the resource.
+   */
+  public function index()
+  {
+    return EventResource::collection(Event::with('user')->get()) ;
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   */
+  public function store(Request $request)
+  {
+    $data = $this->validate($request);
+
+    $event = Event::create([...$data, 'user_id' => 1]);
+
+    return new EventResource($event);
+  }
+
+  /**
+   * Display the specified resource.
+   */
+  public function show(int $id)
+  {
+    return new EventResource(Event::find($id)->load('user', 'attendees'));
+  }
+
+  /**
+   * Update the specified resource in storage.
+   */
+  public function update(Request $request, string $id)
+  {
+    $event = Event::find($id);
+    $data = $this->validate($request, 'sometimes');
+
+    $event->update($data);
+
+    return new EventResource($event);
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   */
+  public function destroy(int $id)
+  {
+    $event = Event::find($id);
+    $eventName = $event->name;
+    $event->delete();
+
+    return response(status: 204);
+  }
+
+  private function validate($request, $rule = 'required') {
+    return $request->validate([
+      'name' => $rule . '|string|max:255',
+      'description' => 'nullable|string',
+      'start_time' => $rule . '|date',
+      'end_time' => $rule . '|date|after:start_time'
+    ]);
+  }
+}
